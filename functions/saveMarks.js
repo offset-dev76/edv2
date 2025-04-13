8ba6f535469306c63844765e9cf678a7eef2c526
offset-dev76 <offset.developer@gmail.com>
2025-04-13 14:12:36 +0530

    Add files via upload

---
+++ functions/saveMarks.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://adityajayaram2468:Adityajrm1124@cluster0.gkmgrrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const markSchema = new mongoose.Schema({
+  student: String,
+  subject: String,
+  marks: Number
+});
+
+const Mark = mongoose.models.Mark || mongoose.model('Mark', markSchema);
+
+let isConnected = false;
+
+async function connectToDB() {
+  if (!isConnected) {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    isConnected = true;
+  }
+}
+
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Only POST requests allowed' })
+    };
+  }
+
+  try {
+    await connectToDB();
+
+    const data = JSON.parse(event.body);
+    const newMark = new Mark(data);
+    await newMark.save();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Mark saved successfully' })
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Server error' })
+    };
+  }
+};
